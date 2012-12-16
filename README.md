a3596c22c2af0981976c7088b3db87ec4f4710b2
Alex Puchkov <alex@a2design.biz>
2012-12-15 21:55:17 -0500

    Add binding, example and readme

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-knockout-orderable
+Knockout orderable binding
 ==================
+This knockout binding allows to create sortable tables. It works by sorting items in observableArray, so tables rendered by "foreach" binding are automatically refreshed by knockout.
 
-A Knockout.js binding to make sortable tables from observableArrays
+##Design Goals
+
+###Focused only on ordering
+Some other plugins take control of entire table rendering, making it complicated to build arbitrary tables. This plugin is only focused on sorting of observableArrays, leaving rendering of HTML to knockout. It gives freedom of using custom item templates for tables or lists.
+
+###Controllable from a view model
+When binding applied for the first time it adds extra observables to a view model. So order can be changed programmatically like 
+
+	viewModel.people.orderDirection("desc") 
+or 
+
+	viewModel.people.orderDirection("lastName")
+
+###Minimal view model setup
+There is nothing to be configured on a view model manually to use the plugin. Only bindings have to be set on elements which will trigger observableArray to be reordered.
+
+###Can be used with multiple observableArrays
+Works well with multiple observableArray. oservableArrays can be ordered independently of each other.
+
+
+##Usage
+To make table header sortable set binding like this:
+
+	<th><a href="#" data-bind="orderable: {collection: 'people', field: 'firstName'}">First Name</a></th>
+
+See full example in examples folder.
+
+##Dependencies
+ - [jQuery](http://jquery.com/)
+ - [Knockout](http://knockoutjs.com/)
+
+##License
+MIT license - [http://www.opensource.org/licenses/mit-license.php](http://www.opensource.org/licenses/mit-license.php)
---
+++ knockout.bindings.orderable.js
@@ -0,0 +1,81 @@
+﻿ko.bindingHandlers.orderable = {
+    compare: function (left, right) {
+        if (left > right)
+            return 1;
+
+        return left < right ? -1 : 0;
+    },
+
+    sort: function (viewModel, collection, field) {
+        //make sure we sort only once and not for every binding set on table header
+        if (viewModel[collection].orderField() == field) {
+            viewModel[collection].sort(function (left, right) {
+                if (viewModel[collection].orderDirection() == "desc") {
+                    return ko.bindingHandlers.orderable.compare(right[field](), left[field]());
+                } else {
+                    return ko.bindingHandlers.orderable.compare(left[field](), right[field]());
+                }
+            });
+        }
+    },
+
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        //get provided options
+        var collection = valueAccessor().collection;
+        var field = valueAccessor().field;
+
+        //add a few observables to ViewModel to track order field and direction
+        if (viewModel[collection].orderField == undefined) {
+            viewModel[collection].orderField = ko.observable();
+        }
+        if (viewModel[collection].orderDirection == undefined) {
+            viewModel[collection].orderDirection = ko.observable("asc");
+        }
+
+        //set order observables on table header click
+        $(element).click(function (e) {
+            e.preventDefault();
+            
+            //flip sort direction if current sort field is clicked again
+            if (viewModel[collection].orderField() == field) {
+                if (viewModel[collection].orderDirection() == "asc") {
+                    viewModel[collection].orderDirection("desc");
+                } else {
+                    viewModel[collection].orderDirection("asc");
+                }
+            }
+            
+            viewModel[collection].orderField(field);
+        });
+
+        //order records when observables changes, so ordering can be changed programmatically
+        viewModel[collection].orderField.subscribe(function () {
+            ko.bindingHandlers.orderable.sort(viewModel, collection, field);
+        });
+        viewModel[collection].orderDirection.subscribe(function () {
+            ko.bindingHandlers.orderable.sort(viewModel, collection, field);
+        });
+    },
+
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        //get provided options
+        var collection = valueAccessor().collection;
+        var field = valueAccessor().field;
+        var isOrderedByThisField = viewModel[collection].orderField() == field;
+            
+        //apply css binding programmatically
+        ko.bindingHandlers.css.update(
+            element,
+            function () {
+                return {
+                    sorted: isOrderedByThisField,
+                    asc: isOrderedByThisField && viewModel[collection].orderDirection() == "asc",
+                    desc: isOrderedByThisField && viewModel[collection].orderDirection() == "desc"
+                };
+            },
+            allBindingsAccessor,
+            viewModel,
+            bindingContext
+        );
+    }
+};
